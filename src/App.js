72f799aa4f064dab0df8d5f941cb2fca53718881
Daniel Addae <nanaayimda@gmail.com>
2018-02-17 14:21:11 +0000

    fixed Router

--- src/App.js
+++ src/App.js
@@ -13,13 +13,15 @@ const Routes = () => (
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Danny's React App, testing Routing on netlify</h1>
-        </header>
-        <Routes />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to Danny's React App, testing Routing on netlify</h1>
+          </header>
+          <Routes />
+        </div>
+      </Router>
     );
   }
 }
